3f31804a69a195483b29cc0deeb1f7406693ba63
maokailu <1113901189@qq.com>
2018-04-22 22:25:33 +0800

    '前后端分离之后端配置'

--- src/main/webapp/components/loading/pip.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import './style.scss';
-export default function Pip(props) {
-  return <div className = {'pip' + props.num}></div>;
-}
--- src/main/webapp/components/overlay/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './style.scss';
-
-
-export default function Overlay() {
-  return (
-    <div className="overlay"></div>
-  );
-}
