2025b0e9411ebfed440496a86f0b057081743842
maokailu <1113901189@qq.com>
2018-04-24 12:52:46 +0800

    add files that would be ignored

--- src/main/java/com/cumt/controller/FlightController.java
+++ src/main/java/com/cumt/controller/FlightController.java
@@ -5,13 +5,18 @@ import com.alibaba.fastjson.JSONObject;
 import com.cumt.pojo.Flight;
 import com.cumt.service.CategoryService;
 import com.cumt.service.FlightService;
+import com.sun.corba.se.spi.activation.LocatorPackage.ServerLocation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 /**
@@ -23,12 +28,13 @@ import java.util.List;
 public class FlightController {
     @Autowired
     FlightService flightService;
-    @RequestMapping("fecthFlightssss")
+    @RequestMapping("fecthFlightsFlight")
     @ResponseBody
-    public String fecthFlights1(HttpServletRequest request, HttpServletResponse response){
+    public String fecthFlights1(HttpServletRequest request, HttpServletResponse response) {
         response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Header", "*");
+//        response.setHeader("Content-Type", "application/json;charset=UTF-8");
         List<Flight> cs = flightService.list();
-        System.out.print("jj");
         return JSONArray.toJSONString(cs);
     }
     @RequestMapping("getLocationFlight")
@@ -37,10 +43,14 @@ public class FlightController {
         response.setHeader("Access-Control-Allow-Origin", "*");
         Flight c = new Flight();
         c.setId(100);
-        c.setName("XuZhouJJ");
+        c.setName("XuZhou");
         JSONObject json= new JSONObject();
         json.put("city", JSONObject.toJSON(c));
         return json.toJSONString();
     }
+//    public static void main(String[] args) {
+//        ServerLocation location = obj.getLocation("206.190.36.45");
+//        System.out.println(location);
+//    }
 }
 
