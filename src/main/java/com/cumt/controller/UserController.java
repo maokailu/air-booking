db5642c76d10bf3be1930d4a90c6588ee57babc5
maokl <maokl@Ctrip.com>
2018-06-07 09:14:48 +0800

    save

--- src/main/java/com/cumt/controller/UserController.java
+++ src/main/java/com/cumt/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
